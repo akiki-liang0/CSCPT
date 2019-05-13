22a24fff802f682a565cb315b8738cf8538210a0
overpaidScrambler <47992181+overpaidScrambler@users.noreply.github.com>
2019-05-13 11:30:30 -0400

    Update main.java

--- main.java
+++ main.java
@@ -3,18 +3,51 @@ import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 
-public class main{
+public class main implements ActionListener,MouseListene,MouseMotionListener,KeyListener{
   //PROPERTIES
   JFrame theframe = new JFrame("");
-  JPanel thepanel = new JPanel();
-  
-  
+  animation thepanel = new animation();
+  Timer thetimer = new Timer(1000/60, this);
+  SuperSocketMaster ssm = new SuperSocketMaster(3000, this);
   //METHODS
+  public void actionPerformed(ActionEvent evt){
+  }
+  public void mouseMoved(MouseEvent evt){
+  }
+  public void mouseDragged(MouseEvent evt){
+  }
+  public void mouseExited(MouseEvent evt){
+  }
+  public void mouseEntered(MouseEvent evt){
+  }
+  public void mousePressed(MouseEvent evt){
+  }
+  public void mouseReleased(MouseEvent evt){
+  }
+  public void mouseClicked(MouseEvent evt){
+  }
+  public void keyReleased(KeyEvent evt){
+  }
+  public void keyTyped(MouseEvent evt){
+  }
+  public void kayPressed(MouseEvent evt){
+  }
   
   
   //CONSTRUCTOR
   public main(){
+    thepanel.setLayout(null);
+    thepanel.setPreferredSize(new Dimension(0,0));
+    thepanel.addMouseListener(this);
+    thepanel.addMouseMotionListener(this);
     
+    theframe.setDefaultCLoseOperation(JFrame.EXIT_ON_CLOSE);
+    theframe.setContentPane(thepanel);
+    theframe.addKeyListener(this);
+    theframe.pack();
+    theframe.setVisible(true);
+    
+    thetimer.start();
     
   }
   
