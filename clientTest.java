047e4fc3a0d8603791add05012a4f495a8998ef7
overpaidScrambler <47992181+overpaidScrambler@users.noreply.github.com>
2019-05-13 11:44:20 -0400

    Update clientTest.java
    -ssm added
      - automate port and ip to main program

--- clientTest.java
+++ clientTest.java
@@ -1,5 +1,37 @@
-public class clientTest{
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
 
+public class clientTest implements actionListener{
+  //PROPERTIES
+  JFrame theframe;
+  animation thepanel;
+  SuperSocketMaster ssm;
+  
+  //METHODS
+  public void actionPerformed(ActionEvent evt){
+    
+    
+  }
+  
+  //CONSTRUCTOR
+  public chatTest(){
+    // panel 
+    thepanel = new animation();
+    thepanel.setLayout(null);
+    thepanel.setPreferredSize(new Dimension(0,0));
+    
+    theframe = new JFrame("");
+    theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    theframe.setConentPane(thepanel);
+    theframe.pack();
+    theframe.setVisible(true);
+    
+    ssm = new SuperSocketMaster(main.strIP, main.intPort, this);
+  }
+  
+  //MAIN METHOD
 
 
 }
