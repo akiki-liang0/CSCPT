496b8dcbcb65e96ea858b845b3df8f6b6cacb6f4
Candace <candacewyng@gmail.com>
2019-05-15 14:18:27 -0400

    Update animation.java
    added background

--- animation.java
+++ animation.java
@@ -1,6 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class animation extends JPanel{
-
-
-
+	// properties
+	
+	
+	// methods
+	public void paintComponent(Graphics g){
+		g.setColor(Color.YELLOW);
+		g.fillRect(0, 0, 1280, 720);
+		g.setColor(Color.WHITE);
+		g.fillRect(10, 60, 675, 600);
+		g.setColor(Color.RED);
+		g.fillRect(820, 10, 450, 345);
+		g.setColor(Color.BLUE);
+		g.fillRect(820, 365, 450, 345);
+	}
+	
+	// constructor
+	public animation(){
+		super();
+	}
 }
+
+
+
