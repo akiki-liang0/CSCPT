dffbec995c5b51b058fa7871ae66e93e0b4456e9
Candace Ng <Candace.Ng19@ycdsbk12.ca>
2019-05-15 19:00:49 -0400

    Update animation.java
    added comments

--- animation.java
+++ animation.java
@@ -2,22 +2,29 @@ import java.awt.*;
 import javax.swing.*;
 
 public class animation extends JPanel{
-	// properties
+	// PROPERTIES
 	
 	
-	// methods
+	// METHODS
 	public void paintComponent(Graphics g){
+		// background
 		g.setColor(Color.YELLOW);
 		g.fillRect(0, 0, 1280, 720);
+		
+		// game board
 		g.setColor(Color.WHITE);
 		g.fillRect(10, 60, 675, 600);
+		
+		// chat area
 		g.setColor(Color.RED);
 		g.fillRect(820, 10, 450, 345);
+		
+		// rankings
 		g.setColor(Color.BLUE);
 		g.fillRect(820, 365, 450, 345);
 	}
 	
-	// constructor
+	// CONSTRUCTOR
 	public animation(){
 		super();
 	}
