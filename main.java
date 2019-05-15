e7ef6558496a56067ada6366f9473042b28d9999
Candace Ng <Candace.Ng19@ycdsbk12.ca>
2019-05-15 17:02:14 -0400

    Update main.java
    fixed the indentation because oCD

--- main.java
+++ main.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 
-public class main implements ActionListener,MouseListener,MouseMotionListener,KeyListener{
-  //PROPERTIES
+public class main implements ActionListener, MouseListener, MouseMotionListener, KeyListener{
+	//PROPERTIES
 	JFrame theframe = new JFrame("can this work thanks");
 	animation thepanel = new animation();
 	JTextArea chat = new JTextArea();
@@ -15,69 +15,66 @@ public class main implements ActionListener,MouseListener,MouseMotionListener,Ke
   
 	int intPort = 3000;
   
-  //METHODS
-  public void actionPerformed(ActionEvent evt){
-  }
-  public void mouseMoved(MouseEvent evt){
-  }
-  public void mouseDragged(MouseEvent evt){
-  }
-  public void mouseExited(MouseEvent evt){
-  }
-  public void mouseEntered(MouseEvent evt){
-  }
-  public void mousePressed(MouseEvent evt){
-  }
-  public void mouseReleased(MouseEvent evt){
-  }
-  public void mouseClicked(MouseEvent evt){
-  }
-  public void keyReleased(KeyEvent evt){
-  }
-  public void keyTyped(KeyEvent evt){
-  }
-  public void keyPressed(KeyEvent evt){
-  }
+	//METHODS
+	public void actionPerformed(ActionEvent evt){
+	}
+	public void mouseMoved(MouseEvent evt){
+	}
+	public void mouseDragged(MouseEvent evt){
+    }
+	public void mouseExited(MouseEvent evt){
+	}
+	public void mouseEntered(MouseEvent evt){
+	}
+	public void mousePressed(MouseEvent evt){
+	}
+	public void mouseReleased(MouseEvent evt){
+	}
+	public void mouseClicked(MouseEvent evt){
+	}
+	public void keyReleased(KeyEvent evt){
+	}
+	public void keyTyped(KeyEvent evt){
+	}
+	public void keyPressed(KeyEvent evt){
+	}
+  
+    //CONSTRUCTOR
+	public main(){
+		// panel
+		thepanel.setLayout(null);
+		thepanel.setPreferredSize(new Dimension(1280, 720));
+		thepanel.addMouseListener(this);
+		thepanel.addMouseMotionListener(this);
+		
+		// frame
+		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theframe.setContentPane(thepanel);
+		theframe.addKeyListener(this);
+		theframe.pack();
+		theframe.setResizable(false);
+		theframe.setVisible(true);
+		
+		// the chat
+		thescroll.setBounds(830, 295, 320, 50);
+		thepanel.add(thescroll);
+		send.setBounds(1160, 295, 100, 50);
+		thepanel.add(send);
+		
+		// super socket master
+		ssm = new SuperSocketMaster(intPort, this);
+		ssm.connect();
+		System.out.println(ssm.getMyAddress());
+		
+		// timer
+		thetimer.start();
+    }
   
   
-  //CONSTRUCTOR
-  public main(){
-    // panel
-    thepanel.setLayout(null);
-    thepanel.setPreferredSize(new Dimension(1280, 720));
-    thepanel.addMouseListener(this);
-    thepanel.addMouseMotionListener(this);
-    
-    // frame
-    theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    theframe.setContentPane(thepanel);
-    theframe.addKeyListener(this);
-    theframe.pack();
-    theframe.setResizable(false);
-    theframe.setVisible(true);
-    
-    // the chat
-	thescroll.setBounds(830, 295, 320, 50);
-	thepanel.add(thescroll);
-	send.setBounds(1160, 295, 100, 50);
-	thepanel.add(send);
-    
-    // super socket master
-    ssm = new SuperSocketMaster(intPort, this);
-    ssm.connect();
-    System.out.println(ssm.getMyAddress());
-    
-    // timer
-    thetimer.start();
-    
-  }
-  
-  
-  //MAIN METHOD
-  public static void main(String[] args){
-    new main();
-  
-  }
+    //MAIN METHOD
+    public static void main(String[] args){
+      new main();
+    }
 
 
 
