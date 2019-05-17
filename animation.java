02606cebdfb9d175ed294c54f46cd0994111513e
Candace <candacewyng@gmail.com>
2019-05-17 14:09:16 -0400

    Update animation.java
    updated colours and added code for the flag

--- animation.java
+++ animation.java
@@ -1,14 +1,27 @@
+import java.io.*;
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.*;
 import javax.swing.*;
 
 public class animation extends JPanel{
 	// PROPERTIES
-	
+	BufferedImage flag;
 	
 	// METHODS
 	public void paintComponent(Graphics g){
+		// this is the flag
+		/*
+		try{
+			flag = ImageIO.read(new File("flag.jpg"));
+		}catch(IOException e){
+			
+		}
+		g.drawImage(flag, 0, 0, null);
+		*/
+		
 		// background
-		g.setColor(Color.YELLOW);
+		g.setColor(new Color(255, 234, 167));
 		g.fillRect(0, 0, 1280, 720);
 		
 		// game board
@@ -16,11 +29,11 @@ public class animation extends JPanel{
 		g.fillRect(10, 60, 675, 600);
 		
 		// chat area
-		g.setColor(Color.RED);
+		g.setColor(new Color(116, 185, 255));
 		g.fillRect(820, 10, 450, 345);
 		
 		// rankings
-		g.setColor(Color.BLUE);
+		g.setColor(new Color(250, 177, 160));
 		g.fillRect(820, 365, 450, 345);
 	}
 	
