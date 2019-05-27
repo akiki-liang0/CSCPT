8f82b59a53513e302cfd9dcb2b1b0fe9a0db4db4
Candace Ng <Candace.Ng19@ycdsbk12.ca>
2019-05-27 14:15:41 -0400

    Update animation.java
    made paths to different screens

--- animation.java
+++ animation.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 
 public class animation extends JPanel{
 	//PROPERTIES
-	BufferedImage flag;
 	BufferedImage gameBoard;
 	BufferedImage gameBoardDark;
 	BufferedImage mainMenu;
@@ -14,13 +13,16 @@ public class animation extends JPanel{
 	BufferedImage helpScreen2;
 	BufferedImage helpScreen3;
 	BufferedImage connectionScreen;
+	BufferedImage settingsScreen;
+	boolean blnGameStart = false;
+	boolean blnSettings = false;
+	boolean blnHelp = false;
 	boolean blnDarkMode = false;
 	
 	//METHODS
 	public void paintComponent(Graphics g){
 		// this is the flag
 		try{
-			flag = ImageIO.read(new File("flag.jpg"));
 			gameBoard = ImageIO.read(new File("gameBoard.png"));
 			gameBoardDark = ImageIO.read(new File("gameBoardDark.png"));
 			// screens 
@@ -29,47 +31,52 @@ public class animation extends JPanel{
 			helpScreen2 = ImageIO.read(new File("Pics/helpScreen2.png"));
 			helpScreen3 = ImageIO.read(new File("Pics/helpScreen3.png"));
 			connectionScreen = ImageIO.read(new File("Pics/connectionScreen.png"));
+			settingsScreen = ImageIO.read(new File("Pics/settingScreen.png"));
 		}catch(IOException e){
 			
 		}
-		//g.drawImage(flag, 0, 0, null);
 		
 		//need to put this first before the other screens
 		g.drawImage(mainMenu, 0, 0, null);
 		
-		// background
-		if(blnDarkMode == true){
+		if(blnSettings == true){
+			g.drawImage(settingsScreen, 0, 0, null);
+		}else if(blnHelp == true){
+			g.drawImage(helpScreen1, 0, 0, null);
+		}else if(blnGameStart == true){
+			// background
+			if(blnDarkMode == true){
+				g.setColor(Color.BLACK);
+			}else{
+				g.setColor(new Color(255, 234, 167));
+			}
+			g.fillRect(0, 0, 1280, 720);
+			
+			// game board
+			if(blnDarkMode == true){
+				g.drawImage(gameBoardDark, 10, 100, null);
+				g.setColor(Color.WHITE);
+				g.drawString("Dark Mode", 640, 28);
+			}else{
+				g.drawImage(gameBoard, 10, 100, null);
+				g.setColor(Color.BLACK);
+				g.drawString("Dark Mode", 640, 28);
+			}
+			//g.setColor(Color.WHITE);
+			//g.fillRect(10, 60, 675, 600);
+			
+			// chat area
+			g.setColor(new Color(116, 185, 255));
+			g.fillRect(820, 10, 450, 345);
 			g.setColor(Color.BLACK);
-		}else{
-			g.setColor(new Color(255, 234, 167));
-		}
-		g.fillRect(0, 0, 1280, 720);
-		
-		// game board
-		if(blnDarkMode == true){
-			g.drawImage(gameBoardDark, 10, 100, null);
-			g.setColor(Color.WHITE);
-			g.drawString("Dark Mode", 640, 28);
-		}else{
-			g.drawImage(gameBoard, 10, 100, null);
+			g.drawString("Chat", 1040, 25);
+			
+			// rankings
+			g.setColor(new Color(250, 177, 160));
+			g.fillRect(820, 365, 450, 345);
 			g.setColor(Color.BLACK);
-			g.drawString("Dark Mode", 640, 28);
+			g.drawString("Rankings", 1025, 380);
 		}
-		//g.setColor(Color.WHITE);
-		//g.fillRect(10, 60, 675, 600);
-		
-		// chat area
-		g.setColor(new Color(116, 185, 255));
-		g.fillRect(820, 10, 450, 345);
-		g.setColor(Color.BLACK);
-		g.drawString("Chat", 1040, 25);
-		
-		// rankings
-		g.setColor(new Color(250, 177, 160));
-		g.fillRect(820, 365, 450, 345);
-		g.setColor(Color.BLACK);
-		g.drawString("Rankings", 1025, 380);
-		
 	}
 	
 	
